125a24d9036eed45f7e03e0875c291c920ae1392
kohyerim <kohyerim78@gmail.com>
2020-06-21 15:28:54 +0900

    review page 설정 및 DB연동

--- src/main/java/kr/ac/jejunu/netflix/WebController.java
+++ src/main/java/kr/ac/jejunu/netflix/WebController.java
@@ -3,15 +3,25 @@ package kr.ac.jejunu.netflix;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequiredArgsConstructor
-public class UserController {
+public class WebController {
     private final UserDao userDao;
 
     @GetMapping("/user/{id}")
     public User get(@PathVariable("id") Integer id){
         return userDao.findById(id).get();
+    }
+
+    @RequestMapping("/review")
+    public ModelAndView test(){
+        ModelAndView modelAndView = new ModelAndView("web");
+        modelAndView.addObject("name", userDao.findById(1).get().getName());
+
+        return modelAndView;
     }
 }
